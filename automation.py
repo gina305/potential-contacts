328bca585ac1a82cf7f07fec400e3af61ac5fffa
gina305 <napier.gina@gmail.com>
2022-08-13 00:47:23 -0400

    Added missing logic and resolved None error

--- automation.py
+++ automation.py
@@ -1,19 +1,26 @@
 import filecmp
 import re
 
-def get_data(file="potential-contacts.txt"):
+#Define gloal variables
+found_contacts = []
+text = ""
+
+def get_data():
+    file="potential-contacts.txt"
     
-    text = ""
+    #Define pattern
     num_pattern = re.compile(r"\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}")
     email_pattern = '\S+@\S+'
     unique = []
+
     #Extract phone numbers
     with open(file) as file:
-        text = file.read()
+        text = file.read().rstrip()
         found_contacts = re.findall(num_pattern, text)
-        found_contacts = found_contacts.sort()
-        
+    
     file.close()
+
+
     res = [] # Used for trcking numbers
 
     dest = open("phone_numbers.txt","a")
@@ -31,23 +38,16 @@ def get_data(file="potential-contacts.txt"):
             
     dest.close()
 
- 
-   
-
     #Write to destination file
     emails = [] 
 
   #Change destination
     dest = open("emails.txt","a") # change file destionation
     with open("potential-contacts.txt") as file:
-        text = file.read()
-        emails = re.findall(email_pattern, text)
-        emails = emails.sort()
-        print(emails)
-        
+     emails = re.findall(email_pattern, text)
     for email in emails:
         if email not in res:
             dest.writelines(str(f'{email}\n'))
             res.append(email)
-
+    dest.close()
 get_data()
