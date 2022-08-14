912899c3ea58364fa24d0e789a40dc1fd7e00b6e
gina305 <napier.gina@gmail.com>
2022-08-14 08:00:35 -0400

    Added missing logic and resolved None error

--- automation.py
+++ automation.py
@@ -9,6 +9,8 @@ When you run this script:
     2. Phone numbers are extracted and stored in phone_numbers.txt
     3. Emails are extracted and stored in emails.txt
 
+* To run this project, ru the automation.py script.
+
 '''
 
 #Define gloal variables
@@ -18,7 +20,7 @@ text = ""
 def get_data():
     file="potential-contacts.txt"
     
-    #Define pattern
+    #Define pattern for mathing
     num_pattern = re.compile(r"\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}")
     email_pattern = '\S+@\S+'
     unique = []
@@ -29,9 +31,7 @@ def get_data():
         found_contacts = re.findall(num_pattern, text)
     
     file.close()
-
-
-    res = [] # Used for trcking numbers
+    res = [] # Used for trcking numbers and emails
 
     dest = open("phone_numbers.txt","a")
     for number in found_contacts:
@@ -41,17 +41,14 @@ def get_data():
         formatted_number = '-'.join([number[:3], number[4:6], number[6:10]])
         print(number, formatted_number)
         
-        #Write to destination file
+        #Write to destination file once
         if number not in res:
             dest.writelines(str(f'{formatted_number}\n'))
             res.append(number)
-            
     dest.close()
 
-    #Write to destination file
     emails = [] 
-
-  #Change destination
+    #Change destination file and write unique emails
     dest = open("emails.txt","a") # change file destionation
     with open("potential-contacts.txt") as file:
      emails = re.findall(email_pattern, text)
